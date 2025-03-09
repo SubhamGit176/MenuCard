6a2417b3c4a520bd4128cb2629b3ed10903cd14a
SubhamGit176 <63771773+SubhamGit176@users.noreply.github.com>
2025-03-09 11:40:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function showAlert(item, price) {
-    alert(`You selected ${item} for ₹${price}. Enjoy your meal!`);
+// Function to Show Item Image in Popup
+function showItem(name, imageUrl) {
+    document.getElementById("popup-title").innerText = name;
+    document.getElementById("popup-img").src = imageUrl;
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to Close Popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
 }
